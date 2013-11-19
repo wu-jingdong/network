8ce50dc51bf92437337a31c563f7c217ddfa8fa6
wujingdong <donwujing@163.com>
2013-11-19 10:24:57 +0800

    add tcp combain-packet function

--- src/org/wjd/net/tcp_udp/tcp/TcpNormalChannel.java
+++ src/org/wjd/net/tcp_udp/tcp/TcpNormalChannel.java
@@ -3,6 +3,9 @@ package org.wjd.net.tcp_udp.tcp;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.wjd.net.common.Loger;
 import org.wjd.net.tcp_udp.BaseChannel;
@@ -124,7 +127,7 @@ public class TcpNormalChannel extends BaseChannel
 		}
 		byte[] res = new byte[length];
 		System.arraycopy(src, 0, res, 0, res.length);
-		parseMessage(res);
+		combainPacket(res);
 		return true;
 	}
 
@@ -133,6 +136,97 @@ public class TcpNormalChannel extends BaseChannel
 	public void setOnConnectionStatusChanged(TcpConnectHandler connectHandler)
 	{
 		this.connectHandler = connectHandler;
+	}
+
+	private List<byte[]> tempReceiveData = new LinkedList<byte[]>();
+
+	/**
+	 * 组包
+	 * 
+	 * @param src
+	 */
+	private void combainPacket(byte[] src)
+	{
+		ByteBuffer buffer = ByteBuffer.wrap(src);
+		int length = src.length;
+		if (tempReceiveData.isEmpty())
+		{
+			short len = buffer.getShort();
+			if (len == length - 2)
+			{
+				parseMessage(src);
+			} else if (len < length - 2)
+			{
+				handleLarger(src);
+			} else if (len > length - 2)
+			{
+				tempReceiveData.add(src);
+			}
+		} else
+		{
+			byte[] tempStore = tempReceiveData.remove(0);
+			ByteBuffer tempStoreBuffer = ByteBuffer.wrap(tempStore);
+			short tempMLen = tempStoreBuffer.getShort();
+			int tempLength = tempStore.length;
+			if (length < (tempMLen + 2) - tempLength)
+			{
+				// src不足以补齐temp中的消息
+				byte[] temp = new byte[tempLength + length];
+				ByteBuffer tempBuffer = ByteBuffer.wrap(temp);
+				tempBuffer.put(tempStore);
+				tempBuffer.put(src);
+				tempReceiveData.add(temp);
+			} else if (length == (tempMLen + 2) - tempLength)
+			{
+				// src正好补齐temp中的消息
+				byte[] temp = new byte[tempLength + length];
+				ByteBuffer tempBuffer = ByteBuffer.wrap(temp);
+				tempBuffer.put(tempStore);
+				tempBuffer.put(src);
+				parseMessage(temp);
+			} else if (length > (tempMLen + 2) - tempLength)
+			{
+				// src补齐temp中的消息后还有剩余
+				byte[] temp = new byte[tempMLen + 2];
+				ByteBuffer tempBuffer = ByteBuffer.wrap(temp);
+				tempBuffer.put(tempStore);
+				int sliceLength = temp.length - tempLength;
+				tempBuffer.put(src, 0, sliceLength);
+				parseMessage(temp);
+				byte[] remain = new byte[length - sliceLength];
+				buffer.get(remain);
+				combainPacket(remain);
+			}
+		}
+	}
+
+	private void handleLarger(byte[] src)
+	{
+		ByteBuffer buffer = ByteBuffer.wrap(src);
+		int length = src.length;
+		// ---1---- 处理第一个完整的消息
+		buffer.mark();
+		int len = buffer.getShort();
+		buffer.reset();
+		byte[] dest = new byte[len + 2];
+		buffer.get(dest, 0, len + 2);
+		parseMessage(dest);
+		// ---2--- 处理后半部分消息
+		buffer.mark();
+		int lLen = buffer.getShort();
+		buffer.reset();
+		byte[] remain = new byte[length - len - 2];
+		buffer.get(remain, 0, length - len - 2);
+		if (lLen < length - len - 4)
+		{
+			handleLarger(remain);
+		} else if (lLen == length - len - 4)
+		{
+			parseMessage(remain);
+		} else if (lLen > length - len - 4)
+		{
+			tempReceiveData.add(remain);
+		}
 	}
 
 	private static final int STATUS_HANDLER = 102;
